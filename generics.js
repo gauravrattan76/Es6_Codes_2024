23075c28338e234356ce567886198f7ae94533e6
gaurav rattan <gauravrattan76@gmail.com>
2019-03-14 22:25:04 +0530

    Generics Commit

---
+++ generics.js
@@ -0,0 +1,5 @@
+function add(a) {
+    return a;
+}
+var myString = add(1);
+console.log(myString);
---
+++ generics.ts
@@ -0,0 +1,6 @@
+function add<T>(a:T):T{
+    return a;
+}
+
+var myString:string= add(1);
+console.log(myString);
