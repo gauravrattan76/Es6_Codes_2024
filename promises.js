c0fd5ece9b3d0fa9896ae37d2e4594421753ca57
gaurav rattan <gauravrattan76@gmail.com>
2019-03-17 15:54:30 +0530

    promise 2nd commit

--- promises.js
+++ promises.js
@@ -1,8 +1,10 @@
 var promise = new Promise(function (resolve, reject) {
     // the function is executed automatically when the promise is constructed
     // after 1 second signal that the job is done with the result "done!"
-    setTimeout(function () { return resolve("done!"); }, 3000);
+    setTimeout(function () { return reject("we have a error"); }, 500);
 });
 promise.then(function (res) {
     console.log(res);
+}, function (err) {
+    console.log(err);
 });
--- promises.ts
+++ promises.ts
@@ -2,9 +2,11 @@ let promise = new Promise(function(resolve, reject) {
     // the function is executed automatically when the promise is constructed
   
     // after 1 second signal that the job is done with the result "done!"
-    setTimeout(() => resolve("done!"), 3000);
+    setTimeout(() => reject("we have a error"), 500);
   });
 
   promise.then(function(res){
    console.log(res);
+  },function(err){
+      console.log(err);
   })
